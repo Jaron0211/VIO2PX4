6c58b22ba4e105dd214698ca40b6a855eb5e2d4f
jaron0211 <jaron0211@gmail.com>
2023-02-06 12:32:02 +0800

    second

---
+++ vio2px4.py
@@ -0,0 +1,99 @@
+#! /usr/bin/env python
+
+import rospy
+from geometry_msgs.msg import PoseStamped
+from mavros_msgs.msg import State
+from mavros_msgs.srv import CommandBool, CommandBoolRequest, SetMode, SetModeRequest
+
+current_state = State()
+
+# Odometry function
+def send_vision_position_estimate_message():
+    global current_time_us, H_aeroRef_aeroBody, reset_counter
+    with lock:
+        if H_aeroRef_aeroBody is not None:
+            # Setup angle data
+            rpy_rad = np.array( tf.euler_from_matrix(H_aeroRef_aeroBody, 'sxyz'))
+
+            # Setup covariance data, which is the upper right triangle of the covariance matrix, see here: https://files.gitter.im/ArduPilot/VisionProjects/1DpU/image.png
+            # Attemp #01: following this formula https://github.com/IntelRealSense/realsense-ros/blob/development/realsense2_camera/src/base_realsense_node.cpp#L1406-L1411
+            cov_pose    = linear_accel_cov * pow(10, 3 - int(data.tracker_confidence))
+            cov_twist   = angular_vel_cov  * pow(10, 1 - int(data.tracker_confidence))
+            covariance  = np.array([cov_pose, 0, 0, 0, 0, 0,
+                                       cov_pose, 0, 0, 0, 0,
+                                          cov_pose, 0, 0, 0,
+                                            cov_twist, 0, 0,
+                                               cov_twist, 0,
+                                                  cov_twist])
+
+            # Send the message
+            conn.mav.vision_position_estimate_send(
+                current_time_us,            # us Timestamp (UNIX time or time since system boot)
+                H_aeroRef_aeroBody[0][3],   # Global X position
+                H_aeroRef_aeroBody[1][3],   # Global Y position
+                H_aeroRef_aeroBody[2][3],   # Global Z position
+                rpy_rad[0],	                # Roll angle
+                rpy_rad[1],	                # Pitch angle
+                rpy_rad[2],	                # Yaw angle
+                covariance,                 # Row-major representation of pose 6x6 cross-covariance matrix
+                reset_counter               # Estimate reset counter. Increment every time pose estimate jumps.
+            )
+
+def state_cb(msg):
+    global current_state
+    current_state = msg
+
+
+if __name__ == "__main__":
+    rospy.init_node("offb_node_py")
+
+    state_sub = rospy.Subscriber("mavros/state", State, callback = state_cb)
+
+    local_target_pub = rospy.Publisher("mavros/setpoint_position/local", PoseStamped, queue_size=10)
+    
+    rospy.wait_for_service("/mavros/cmd/arming")
+    arming_client = rospy.ServiceProxy("mavros/cmd/arming", CommandBool)    
+
+    rospy.wait_for_service("/mavros/set_mode")
+    set_mode_client = rospy.ServiceProxy("mavros/set_mode", SetMode)
+
+    local_pos_pub = rospy.Publisher("mavros/vision_position_estimate", PoseStamped, queue_size=10)
+    
+
+    # Setpoint publishing MUST be faster than 2Hz
+    rate = rospy.Rate(30)
+
+    # Wait for Flight Controller connection
+    while(not rospy.is_shutdown() and not current_state.connected):
+        rate.sleep()
+
+
+    offb_set_mode = SetModeRequest()
+    offb_set_mode.custom_mode = 'POSITION'
+
+    arm_cmd = CommandBoolRequest()
+    arm_cmd.value = True
+
+    last_req = rospy.Time.now()
+
+    while(not rospy.is_shutdown()):
+        if(current_state.mode != "OFFBOARD" and (rospy.Time.now() - last_req) > rospy.Duration(5.0)):
+            if(set_mode_client.call(offb_set_mode).mode_sent == True):
+                rospy.loginfo("OFFBOARD enabled")
+            
+            last_req = rospy.Time.now()
+        else:
+            if(not current_state.armed and (rospy.Time.now() - last_req) > rospy.Duration(5.0)):
+                if(arming_client.call(arm_cmd).success == True):
+                    rospy.loginfo("Vehicle armed")
+            
+                last_req = rospy.Time.now()
+                pose = PoseStamped()
+                pose.pose.position.x = 0
+                pose.pose.position.y = 0
+                pose.pose.position.z = 2
+                local_target_pub.publish(pose)
+
+        
+        rate.sleep()
+
